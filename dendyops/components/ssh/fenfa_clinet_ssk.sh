80fa0a26166930d289ce87b4760588a8cf34bf24
jie cao <1009760106@qq.com>
2021-04-03 23:51:51 +0800

    update 2351

--- dendyops/components/ssh/fenfa_clinet_ssk.sh
+++ dendyops/components/ssh/fenfa_clinet_ssk.sh
@@ -2,16 +2,18 @@
 . /etc/init.d/functions
 function a_sub(){
                 local ip=$1
-                local passwd
-                expect ./fenfa_clinet_sshkey.exp  ~/.ssh/id_rsa.pub  $ip $passwd >/dev/null 2>&1
+                local passwd=$2
+                expect /opt/dendyops/components/ssh/fenfa_clinet_sshkey.exp  ~/.ssh/id_rsa.pub  $ip $passwd >/dev/null 2>&1
                 if [ $? -eq 0 ]
                         then
                         action "$ip" /bin/true
                 else
                         action "$ip" /bin/false
+                        echo " \[\e[32;1m\]if  you  See let U try  ssh  it's ok  \[\e[0m\] "
                 fi
+
 }
-if [  ! -z $1 ];then
+if [   -z $1 ];then
     echo  "usage   $@  password "
     exit 1
 fi
