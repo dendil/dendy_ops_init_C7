e725835fe68a22f8935175bcf9d049b8d5852f7f
jie cao <1009760106@qq.com>
2021-05-01 19:29:40 +0800

    modify sethostname

--- dendyops/components/utils/set_hostname.sh
+++ dendyops/components/utils/set_hostname.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-IP=`ip addr show | grep -v 'docker0' | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+IP=`ip addr show | grep -Ev  'docker0|veth|br*:' | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 if [[ "$IP" = "" ]]; then
         IP=`wget -qO- -t1 -T2 ipv4.icanhazip.com`
 fi
