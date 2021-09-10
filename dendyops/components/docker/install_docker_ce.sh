2f3de39d8700862b40d4e642dcd455fcbd55c3f2
dendil <5654635+dendil@user.noreply.gitee.com>
2021-09-10 16:40:18 +0800

    update

--- dendyops/components/docker/install_docker_ce.sh
+++ dendyops/components/docker/install_docker_ce.sh
@@ -24,18 +24,19 @@ fi
 
 innerip=$(ip a  |egrep 'eth0|ens32|ens33|eno1|eno2' |grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' |awk '{print $2}')
 _bip_4=` echo $innerip|awk -F. '{print$4}'`
+mkdir -p /etc/docker
 cat > /etc/docker/daemon.json << EOF
 {
-   "graph": "/data/docker",
+   "graph": "/home/docker",
    "storage-driver": "overlay2",
    "insecure-registries": ["registry.access.redhat.com","quay.io","harbor.od.com"],
    "registry-mirrors": ["https://jltw059v.mirror.aliyuncs.com"],
-   "bip": "10.10.${_bip_4}.1/24",  
+   "bip": "172.16.${_bip_4}.1/24",  
    "exec-opts": ["native.cgroupdriver=systemd"],
    "live-restore": true
 }
 EOF
-mkdir /data/docker -p
+mkdir /home/docker -p
 ./post_install_docker.sh
 
 ../utils/start_service.sh docker
