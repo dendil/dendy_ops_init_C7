65a92089a71704e173a892ad2d770e66175320c8
dendil <5654635+dendil@user.noreply.gitee.com>
2022-11-18 16:09:50 +0800

    update yum

--- dendyops/components/yum/use_huawei_yum_repo.sh
+++ dendyops/components/yum/use_huawei_yum_repo.sh
@@ -12,7 +12,7 @@ fi
 #curl -o /etc/yum.repos.d/CentOS7-Aliyun.repo https://raw.githubusercontent.com/cookcodeblog/OneDayDevOps/master/components/aliyun/CentOS7-Aliyun.repo
 #curl -o /etc/yum.repos.d/epel-7-Aliyun.repo https://raw.githubusercontent.com/cookcodeblog/OneDayDevOps/master/components/aliyun/epel-7-Aliyun.repo
 
-cp CentOS7-Base-huawei.repo /etc/yum.repos.d/
+cp CentOS-Base-huawei.repo /etc/yum.repos.d/
 cp epel-huawei.repo /etc/yum.repos.d/
 
 yum clean all
