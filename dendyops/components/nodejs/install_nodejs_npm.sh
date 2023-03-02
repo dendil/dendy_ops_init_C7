113985b07b1ed74b1cbbad4f5f3de62526fade0f
dendil <5654635+dendil@user.noreply.gitee.com>
2023-03-02 13:33:10 +0800

    update install node

--- dendyops/components/nodejs/install_nodejs_npm.sh
+++ dendyops/components/nodejs/install_nodejs_npm.sh
@@ -9,15 +9,18 @@ then
   cd ${SCRIPT_DIR}
 fi
 
-# Instll nodejs and npm
-sudo yum install epel-release -y
-sudo yum install nodejs
-node -v
-sudo yum install npm -y
-npm -v
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm  install 16 
+nvm  use     16
 
 
 
-# Use Taobao npm registry
-npm config set registry http://r.cnpmjs.org/
-npm config get registry
+
+
